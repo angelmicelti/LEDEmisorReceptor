cb9d5c29583ee570cdb4a2679ac2532244e3e801
angelmicelti <angelmicelti@users.noreply.github.com>
2026-04-29 23:15:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //  SERVICE WORKER - LED Remoto PWA
 // =========================================================
 
-const CACHE_NAME = 'led-remoto-v2.4';
+const CACHE_NAME = 'led-remoto-v2.5';
 
 // Recursos que se cachean en la instalacion
 const PRECACHE_ASSETS = [
